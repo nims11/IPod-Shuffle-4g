64617c4d8ccf7a27d61491f384c480539b966ddd
NicoHood <NicoHood@users.noreply.github.com>
2016-04-05 20:55:44 +0200

    Added script description

--- shuffle.py
+++ shuffle.py
@@ -606,7 +606,9 @@ def handle_interrupt(signal, frame):
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, handle_interrupt)
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description=
+    'Python script for building the Track and Playlist database '
+    'for the newer gen IPod Shuffle.')
     parser.add_argument('--disable-voiceover', action='store_true', help='Disable voiceover feature')
     parser.add_argument('--rename-unicode', action='store_true', help='Rename files causing unicode errors, will do minimal required renaming')
     parser.add_argument('--track-gain', type=nonnegative_int, default=0, help='Specify volume gain (0-99) for all tracks; 0 (default) means no gain and is usually fine; e.g. 60 is very loud even on minimal player volume')
